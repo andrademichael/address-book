9e5621ed2fe9d3af4bcec8726f1fa4db1721ce4a
Jeremy Fryd and Michael Andrade <jeremy@>
2016-08-22 11:48:39 -0700

    add functionality to Another Adress button

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,40 @@ $(document).ready(function() {
   // constructor
   // prototype Contact [names, street, city, state]
   // //Frontend
-
+  function Contact(first, last, street, city, state) {
+    this.first = first;
+    this.last = last;
+    this.street = street;
+    this.city = city;
+    this.state = state;
+  };
+  // var newContact = Contact(firstName, lastName)
 //  form input
-var contactSections = ["firstName", "lastName", "street", "city", "state"]
+  var contactSections = ["firstName", "lastName", "street", "city", "state"]
+  var counter = 0;
+
+  $("#add-address").click(function(event) {
+    $("#addressSection").append('<div class="form-group">' +
+      '<label for="new-street">Street</label>' +
+      '<input class="form-control new-street" type="text">' +
+    '</div>' +
+    '<div class="form-group">' +
+      '<label for="new-city">City</label>' +
+      '<input class="form-control new-city" type="text">' +
+    '</div>' +
+    '<div class="form-group">' +
+      '<label for="new-state">State</label>' +
+      '<input class="form-control new-state" type="text">' +
+    '</div>');
+
+
+
+  });
 
 
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
+    counter += 1;
     var lastName = $("#new-lastName").val();
     var firstName = $("#new-firstName").val();
   if (!(lastName || firstName)) {
@@ -18,6 +45,11 @@ var contactSections = ["firstName", "lastName", "street", "city", "state"]
   } else {
     console.log(firstName + lastName);
     $("#contactList").append("<li>" + firstName + " " + lastName + "</li>");
+    // var ["firstName + lastName"] = new Contact(firstName,lastName);
+    // console.log("firstName + lastName");
+    // console.log(firstName + lastName);
+
+
 
 
     //the rest
